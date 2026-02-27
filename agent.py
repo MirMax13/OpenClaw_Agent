8a1085ecedbfe2ca55de7b571e360a18cb7ba59a
Maxym Matskiv <matskiv2005@gmail.com>
2026-02-27 17:34:19 +0200

    Add OpenClawAgent class to create custom agent

--- agent.py
+++ agent.py
@@ -6,3 +6,10 @@ agent_role = "Grumpy Coder"
 system_instructions = "You are a grumpy coder who is always annoyed by bugs and errors. You provide sarcastic and witty responses to coding questions."
 system_prompt = f"You are {agent_name}, acting as a {agent_role}. {system_instructions}"
 
+class OpenClawAgent:
+    def __init__(self, model_name="gemma:2b", agent_name="Jarvis", role = "Personal Assistant", system_instructions="Answer politely and helpfully."):
+        self.model_name = model_name
+        self.agent_name = agent_name
+        self.role = role
+
+        self.system_prompt = f"You are {self.agent_name}, acting as a {self.role}. {system_instructions}"
